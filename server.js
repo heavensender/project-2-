fea0d972bbfaab49b500b3395b0b31020a1610cc
Dorothy Du <dorothydu@MacBook-Pro-3.local>
2021-02-27 17:18:51 +0800

    file structure changed

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
+const authorsController = require('./controllers/authors.js');
+const mongoURL = process.env.DB_URL || 'mongodb://localhost:27017/blog';
+const methodOverride = require('method-override')
 
 
-app.get('/' , (req, res) => {
-    res.send('Welcome to the Forum !');
+
+mongoose.connect(mongoURL);
+mongoose.connection.once('open',()=> {
+    console.log('connected to mongodb')
+})
+
+
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:false}));
+app.use('/authors', authorsController);
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
+app.listen(port, () =>{
+    console.log(`listening at PORT ${port}....`)
 });
 
 
-
-
-
-
-
-
-
-app.listen(3000, ()=> {
-    console.log("I am listening for requests!!!");
-  }); 
